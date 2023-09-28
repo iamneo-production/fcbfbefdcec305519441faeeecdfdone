369479e890bf7ea385ccf77fcd4db8367438066a
Iamneo <techadmin@iamneo.ai>
2023-09-28 06:31:45 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/model/Laptop.java
+++ springapp/src/main/java/com/examly/springapp/model/Laptop.java
@@ -14,32 +14,28 @@ public class Laptop {
         this.laptopPrice = laptopPrice;
     }
 
-    // Getter method for laptopId
+    // Getter methods
     public int getLaptopId() {
         return laptopId;
     }
 
-    // Setter method for laptopId
-    public void setLaptopId(int laptopId) {
-        this.laptopId = laptopId;
-    }
-
-    // Getter method for laptopBrand
     public String getLaptopBrand() {
         return laptopBrand;
     }
 
-    // Setter method for laptopBrand
-    public void setLaptopBrand(String laptopBrand) {
-        this.laptopBrand = laptopBrand;
-    }
-
-    // Getter method for laptopPrice
     public int getLaptopPrice() {
         return laptopPrice;
     }
 
-    // Setter method for laptopPrice
+    // Setter methods
+    public void setLaptopId(int laptopId) {
+        this.laptopId = laptopId;
+    }
+
+    public void setLaptopBrand(String laptopBrand) {
+        this.laptopBrand = laptopBrand;
+    }
+
     public void setLaptopPrice(int laptopPrice) {
         this.laptopPrice = laptopPrice;
     }
